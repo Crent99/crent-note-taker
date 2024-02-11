a33eeb1005d3b03b37b191ea59048b4cf157879e
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2024-02-11 08:41:24 -0500

    deleted code

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index');
+const fs = require('fs');
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/api', api);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+}
+);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'db/db.json'));
+}
+);
 
-app.get('/notes', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    notes.push(newNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(notes));
+    res.json(newNote);
+}
+);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/404.html'))
-});
-
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+}
+);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
-});
+}
+);
