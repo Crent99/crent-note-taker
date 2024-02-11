86aa8aa8236c6ae4f764b3e0b195bf7dc6c304a0
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2024-02-11 08:03:59 -0500

    added to server

--- server.js
+++ server.js
@@ -3,3 +3,28 @@ const path = require('path');
 const api = require('./routes/index');
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/api', api);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/404.html'))
+});
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
